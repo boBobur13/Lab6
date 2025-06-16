3acb4aca3d10c215ff70e030a030dd63c375905c
Brain17 <b.daminov@newuu.uz>
2025-06-16 15:32:51 +0500

    Add files via upload

---
+++ Lab.6_no1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+double product ( double num1,double num2){
+  return num1*num2;
+}
+int main() 
+{
+    double a,b;
+    cin>>a>>b;
+    cout << product ( num1:a,num2:b)<<endl;
+    return 0;
+}
